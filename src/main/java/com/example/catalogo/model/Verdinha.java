d5413fb6b84e70ef0ddab7bd4aad63cd439cef10
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-23 20:17:14 -0300

    criando os atributos da classe verdinha

--- src/main/java/com/example/catalogo/model/Verdinha.java
+++ src/main/java/com/example/catalogo/model/Verdinha.java
@@ -1,7 +1,15 @@
 package com.example.catalogo.model;
 
+import java.time.LocalDate;
+
 public class Verdinha {
 
-	
+	private String nome;
+	private String especie;
+	private String genero;
+	private String nomePopular;
+	private LocalDate dataAquisicao = LocalDate.now();
+	private boolean visivel;
+	private byte[] fotos;
 	
 }
