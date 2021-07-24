325f353a96bcad88ab9e52d510dd27aae9d95e57
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-24 19:40:00 -0300

    criando o pacote base e a classe entidadebase

---
+++ src/main/java/com/example/base/EntidadeBase.java
@@ -0,0 +1,11 @@
+package com.example.base;
+
+import java.time.LocalDateTime;
+
+public class EntidadeBase {
+
+	private Long id;
+	private LocalDateTime dataInclusao;
+	private LocalDateTime dataUltimaAlteracao;
+	
+}
