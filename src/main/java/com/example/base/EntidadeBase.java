4bd69ae8bffc7a4996b1f165e683d56721b2694e
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-24 21:01:34 -0300

    concluido o mapeamento da classe entidade base e verdinha

--- src/main/java/com/example/base/EntidadeBase.java
+++ src/main/java/com/example/base/EntidadeBase.java
@@ -2,18 +2,28 @@ package com.example.base;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @MappedSuperclass
+@Getter
+@Setter
 public class EntidadeBase {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@Column(nullable = false)
 	private LocalDateTime dataInclusao;
+	
+	@Column(nullable = false)
 	private LocalDateTime dataUltimaAlteracao;
 	
 }
--- src/main/java/com/example/catalogo/model/Verdinha.java
+++ src/main/java/com/example/catalogo/model/Verdinha.java
@@ -5,22 +5,38 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import com.example.base.EntidadeBase;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "verdinhas")
+@Setter
+@Getter
 public class Verdinha extends EntidadeBase {
 
 	@Column(length = 60, nullable = false)
-	//@NotBlank(message = "Informe o nome do vendedor")
-	//@Size(max = 60, min = 2, message = "O nome deve ter no máximo {max} 60 caracteres e no mínimo {min} 2 caracteres")
+	@NotBlank(message = "Informe o nome da planta")
+	@Size(max = 60, min = 2, message = "O nome deve ter no máximo {max} 60 caracteres e no mínimo {min} 2 caracteres")
 	private String nome;
 	
+	@Column(length = 40, nullable = false)
+	@NotBlank(message = "Informe o nome da especie")
+	@Size(max = 40, min = 2, message = "O nome deve ter no máximo {max} 40 caracteres e no mínimo {min} 2 caracteres")
 	private String especie;
 	
+	@Column(length = 30, nullable = false)
+	@NotBlank(message = "Informe o gênero")
+	@Size(max = 30, min = 2, message = "O nome deve ter no máximo {max} 30 caracteres e no mínimo {min} 2 caracteres")
 	private String genero;
 	
+	@Column(length = 40, nullable = false)
+	@NotBlank(message = "Informe o nome popular")
+	@Size(max = 40, min = 2, message = "O nome deve ter no máximo {max} 40 caracteres e no mínimo {min} 2 caracteres")
 	private String nomePopular;
 	
 	@Column(nullable = false)
