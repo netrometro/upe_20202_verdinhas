efd3a23706d3292e728ec393a86942aeb301a47b
maycon santana <maycon_gus2009@hotmail.com>
2021-08-16 23:47:58 -0300

    implementando a classe crud service no pacote base

---
+++ src/main/java/com/example/base/CrudService.java
@@ -0,0 +1,43 @@
+package com.example.base;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import javax.persistence.criteria.Fetch;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CrudService<T extends GenericEntity, ID> {
+
+	<Dao extends JpaRepository<T, ID>> Dao getDao();
+
+	default T insert(T entity) {
+		entity.setDataInclusao(LocalDateTime.now());
+		final T saved = getDao().save(entity);
+		return saved;
+	}
+
+	default T update(T entity) {
+		entity.setDataInclusao(LocalDateTime.now());
+		final T saved = getDao().save(entity);
+		return saved;
+	}
+
+	default T delete(ID id) {
+		final Optional<T> found = getDao().findById(id);
+		if (found.isPresent()) {
+			getDao().delete(found.get());
+			return found.get();
+		}
+		return null;
+	}
+	
+	default Iterable<T> fetchAll() {
+		return getDao().findAll();
+	}
+	
+	default T fetch(ID id) {
+		return getDao().findById(id).get();
+	}
+	
+}
